be35bace3ccb94fb43a5f5b195026cdad60d79ae
Jeff Sternberg <jeffsternberg@gmail.com>
2017-10-01 15:41:40 -0400

    revert staticfiles_dirs

--- fake_news_links/settings.py
+++ fake_news_links/settings.py
@@ -24,7 +24,6 @@ def get_required_env_var(name):
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -143,7 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 REST_FRAMEWORK = {
